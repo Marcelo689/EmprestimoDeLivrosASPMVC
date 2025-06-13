cf8817ae3f600137929c46d51cfdf51a1493f906
Marcelo689 <marcelosj7159@gmail.com>
2025-06-13 18:00:04 -0300

    first push

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,21 @@
+﻿using EmprestimoLivros.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private ApplicationDbContext _db { get; }
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            var listaEmprestimos = _db.Emprestimos.ToList();
+            return View(listaEmprestimos);
+        }
+    }
+}
